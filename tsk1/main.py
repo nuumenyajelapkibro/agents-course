f7f0c4790c53cc6cfbca2f7dfade6a953803a783
Nuumenyajelapkibro <arnautartyom@gmail.com>
2025-08-11 14:49:54 +0300

    Added README

--- tsk1/main.py
+++ tsk1/main.py
@@ -5,15 +5,19 @@ from pydantic import BaseModel, Field
 from pydantic_ai import Agent
 from simpleeval import simple_eval
 
+# получаем ключи API из переменных окружения
 load_dotenv()
 
+# подключаемся к Logfire для логирования и мониторинга
 logfire.configure(token=os.getenv("LOGFIRE_KEY"))
 logfire.instrument_pydantic_ai()
 
+# описывем модель ответа
 class ModelOutput(BaseModel):
     result: float = Field(description="Результат вычислений")
     explanation: str = Field(description="Результаты проверки и пояснение")
 
+# инициализируем агента
 agent = Agent(
     model='openai:gpt-3.5-turbo',
     system_prompt=(
@@ -23,18 +27,20 @@ agent = Agent(
         "Если результат инструмента не совпал с ожидаемым, ты должен повторить попытку. "
         "После успешной проверки ты даешь ответ в формате Pydantic-модели."
     ),
-    output_type=ModelOutput,
+    output_type=ModelOutput, # подключаем модель ответа
+    retries=2, # настраиваеаем количество ретраев
     instrument=True
 )
 
+# инициализируем инструменты агента
 @agent.tool
 def calculate(ctx, expression: str) -> float:
     """Вычисляет значение математического выражения."""
     try:
-        return float(simple_eval(expression))
+        return float(simple_eval(expression)) # используем simple_eval для безопасного вычисления
     except Exception as e:
         raise ValueError(f"Ошибка вычисления: {e}")
 
-response = agent.run_sync("Сколько будет 3*3+1?")
+response = agent.run_sync("Сколько будет 418*2/4?")
 print(f"Результат: {response.output.result}")
 print(f"Пояснение: {response.output.explanation}")
